ca74b44cc3604ebac88bc1ad1fe657548be4b6e8
brupadiro <brunodiharce@gmail.com>
2022-04-28 14:04:21 -0300

    Cambio url nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://api.abunda.uy',
+    baseURL: 'https://api.descuentosya.uy/',
     //baseURL: 'http://localhost:1337/'
   },
   auth: {
